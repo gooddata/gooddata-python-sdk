0a7c0a70cd488f5717250eaef179cbd48d686e1a
Miroslav Čech <miroslav.cech@gooddata.com>
2021-12-20 15:29:48 +0100

    Fix pandas docs to comply with rst

--- gooddata-pandas/gooddata_pandas/series.py
+++ gooddata-pandas/gooddata_pandas/series.py
@@ -23,23 +23,26 @@ class SeriesFactory:
     ) -> pandas.Series:
         """
         Creates pandas Series from data points calculated from a single `data_by` that will be computed on granularity
-        of the index  labels. The elements of the index labels will be used to construct simple or hierarchical index.
+        of the index labels. The elements of the index labels will be used to construct simple or hierarchical index.
 
         :param index_by: label to index by; specify either:
+
          - string with id: 'some_label_id',
          - object identifier: ObjId(id='some_label_id', type='label'),
          - string representation of object identifier: 'label/some_label_id'
          - or an Attribute object used in the compute model: Attribute(local_id=..., label='some_label_id')
          - dict containing mapping of index name to label to use for indexing - specified in one of the ways list above
+
         :param data_by: label, fact or metric to that will provide data (metric values or label elements); specify
          either:
+
          - object identifier: ObjId(id='some_id', type='<type>') - where type is either 'label', 'fact' or 'metric'
          - string representation of object identifier: '<type>/some_id' - where type is either 'label', 'fact' or
            'metric'
          - Attribute object used in the compute model: Attribute(local_id=..., label='some_label_id')
          - SimpleMetric object used in the compute model: SimpleMetric(local_id=..., item=..., aggregation=...)
+
         :param filter_by: optionally specify filter to apply during computation on the server
-        :return:
         """
         data, index = compute_and_extract(
             self._sdk,
@@ -66,21 +69,24 @@ class SeriesFactory:
         Note that data_by may also be a label in which case the Series will contain label elements.
 
         :param data_by: label, fact or metric to get data from; specify either:
+
          - object identifier: ObjId(id='some_id', type='<type>') - where type is either 'label', 'fact' or 'metric'
          - string representation of object identifier: '<type>/some_id' - where type is either 'label', 'fact' or
            'metric'
          - Attribute object used in the compute model: Attribute(local_id=..., label='some_label_id')
          - SimpleMetric object used in the compute model: SimpleMetric(local_id=..., item=..., aggregation=...)
+
         :param granularity: optionally specify label to slice the metric by; specify either:
+
          - string with id: 'some_label_id',
          - object identifier: ObjId(id='some_label_id', type='label'),
          - string representation of object identifier: 'label/some_label_id'
          - or an Attribute object used in the compute model: Attribute(local_id=..., label='some_label_id')
          - list containing multiple labels to slice the metric by - specified in one of the ways list above
          - dict containing mapping of index name to label to use for indexing - specified in one of the ways list above;
-            this option is available so that you can easily switch from indexed factory method to this one if needed
+           this option is available so that you can easily switch from indexed factory method to this one if needed
+
         :param filter_by: optionally specify filter to apply during computation on the server
-        :return:
         """
 
         if isinstance(granularity, list):
