c058f36f96d06bcbc9241071937287eedf15a440
Pavel Cerny <pavel.cerny@gooddata.com>
2022-01-05 12:37:24 +0100

    NAS-353 separate fdw table execution code
    Code extracted from fdw.py and prepared for simplification
    of fdw.py itself. Tests added.

---
+++ gooddata-fdw/gooddata_fdw/executor.py
@@ -0,0 +1,126 @@
+# (C) 2022 GoodData Corporation
+from __future__ import annotations
+
+from typing import Any, Generator, NamedTuple, Optional
+
+import gooddata_fdw.column_utils as column_utils
+import gooddata_fdw.column_validation as col_val
+from gooddata_fdw.environment import ColumnDefinition, Qual
+from gooddata_fdw.filter import extract_filters_from_quals
+from gooddata_fdw.options import ServerOptions, TableOptions
+from gooddata_fdw.result_reader import InsightTableResultReader, TableResultReader
+from gooddata_sdk.sdk import GoodDataSdk
+
+
+class InitData(NamedTuple):
+    sdk: GoodDataSdk
+    server_options: ServerOptions
+    table_options: TableOptions
+    columns: dict[str, ColumnDefinition]
+
+
+class Executor:
+    def __init__(self, inputs: InitData, column_validators: list[col_val.ColumnValidator]) -> None:
+        self._sdk = inputs.sdk
+        self._table_columns = inputs.columns
+        self._column_validators = column_validators
+
+    @classmethod
+    def can_react(cls, inputs: InitData) -> bool:
+        return False
+
+    def validate_columns_def(self) -> None:
+        for column_name, column_def in self._table_columns.items():
+            for validator in self._column_validators:
+                validator.validate(column_name, column_def)
+
+    def execute(
+        self, quals: list[Qual], columns: list[str], sort_keys: Optional[list[Any]] = None
+    ) -> Generator[dict[str, Any], None, None]:
+        raise NotImplementedError()
+
+
+class InsightExecutor(Executor):
+    _COLUMN_VALIDATORS = [col_val.LocalIdOptionValidator(), col_val.IdOptionValidator(mandatory=False)]
+
+    def __init__(self, inputs: InitData) -> None:
+        super().__init__(inputs, self._COLUMN_VALIDATORS)
+        self._workspace = inputs.table_options.workspace
+
+        assert inputs.table_options.insight is not None
+        self._insight = inputs.table_options.insight
+
+        self._table_columns = inputs.columns
+
+    @classmethod
+    def can_react(cls, inputs: InitData) -> bool:
+        return inputs.table_options.insight is not None
+
+    def execute(
+        self, quals: list[Qual], columns: list[str], sort_keys: Optional[list[Any]] = None
+    ) -> Generator[dict[str, Any], None, None]:
+        results_reader = InsightTableResultReader(self._table_columns, columns)
+        insight = self._sdk.insights.get_insight(self._workspace, self._insight)
+        table = self._sdk.tables.for_insight(self._workspace, insight)
+
+        return results_reader.read_all_rows(table)
+
+
+class ComputeExecutor(Executor):
+    _COLUMN_VALIDATORS: list[col_val.ColumnValidator] = [col_val.IdOptionValidator(mandatory=True)]
+
+    def __init__(self, inputs: InitData) -> None:
+        super().__init__(inputs, self._COLUMN_VALIDATORS)
+        self._workspace = inputs.table_options.workspace
+        self._results_reader = TableResultReader(self._table_columns)
+
+    @classmethod
+    def can_react(cls, inputs: InitData) -> bool:
+        return inputs.table_options.compute is not None
+
+    def execute(
+        self, quals: list[Qual], columns: list[str], sort_keys: Optional[list[Any]] = None
+    ) -> Generator[dict[str, Any], None, None]:
+        col_val.validate_columns_in_table_def(self._table_columns, columns)
+        items = [column_utils.table_col_as_computable(self._table_columns[col_name]) for col_name in columns]
+        # TODO: push down more filters that are included in quals
+        filters = extract_filters_from_quals(quals, self._table_columns)
+        table = self._sdk.tables.for_items(self._workspace, items, filters)
+
+        return self._results_reader.read_all_rows(table)
+
+
+class CustomExecutor(Executor):
+    _COLUMN_VALIDATORS: list[col_val.ColumnValidator] = [col_val.IdOptionValidator(mandatory=True)]
+
+    def __init__(self, inputs: InitData) -> None:
+        super().__init__(inputs, self._COLUMN_VALIDATORS)
+        self._workspace = inputs.table_options.workspace
+        self._results_reader = TableResultReader(self._table_columns)
+
+    @classmethod
+    def can_react(cls, inputs: InitData) -> bool:
+        return True
+
+    def execute(
+        self, quals: list[Qual], columns: list[str], sort_keys: Optional[list[Any]] = None
+    ) -> Generator[dict[str, Any], None, None]:
+        items = [column_utils.table_col_as_computable(col) for col in self._table_columns.values()]
+        # TODO: pushdown more filters that are included in quals
+        filters = extract_filters_from_quals(quals, self._table_columns)
+        table = self._sdk.tables.for_items(self._workspace, items, filters)
+
+        return self._results_reader.read_all_rows(table)
+
+
+class ExecutorFactory:
+    # Order is important - first executor supporting InitData is used
+    _SUPPOERTED_EXECUTORS = [InsightExecutor, ComputeExecutor, CustomExecutor]
+
+    @classmethod
+    def create(cls, inputs: InitData) -> Executor:
+        for executor in cls._SUPPOERTED_EXECUTORS:
+            if executor.can_react(inputs):
+                return executor(inputs)
+
+        raise ValueError(f"No executor supports initial data {str(inputs)}")
---
+++ gooddata-fdw/tests/unit_test/test_result_reader.py
@@ -0,0 +1,81 @@
+# (C) 2022 GoodData Corporation
+import unittest.mock as mock
+from collections import OrderedDict
+from datetime import date
+
+import pytest
+
+from gooddata_fdw.environment import ColumnDefinition
+from gooddata_fdw.result_reader import InsightTableResultReader, TableResultReader
+
+
+@pytest.fixture
+def table_columns():
+    columns = OrderedDict()
+    columns["coverage_lifetime"] = ColumnDefinition(
+        column_name="coverage_lifetime",
+        type_name="DECIMAL(15,5)",
+        options=dict(id="fact/coverage.coverage_lifetime", local_id="1"),
+    )
+    columns["claim_amount"] = ColumnDefinition(
+        column_name="claim_amount",
+        type_name="DECIMAL(15,5)",
+        options=dict(id="metric/claim-amount", local_id="2"),
+    )
+    columns["car_make"] = ColumnDefinition(
+        column_name="car_make",
+        type_name="VARCHAR(255)",
+        options=dict(id="label/car.car_make", local_id="3"),
+    )
+    columns["datetime"] = ColumnDefinition(
+        column_name="datetime",
+        type_name="DATE",
+        options=dict(id="label/datetime.day", local_id="4"),
+    )
+
+    return columns
+
+
+def test_table_result_reader(table_columns):
+    executor_output = [
+        {
+            "coverage_lifetime": 125.5,
+            "claim_amount": "55.6",
+            "car_make": "A",
+            "datetime": "2021-03-15",
+        },
+    ]
+    expected = [
+        {
+            "coverage_lifetime": 125.5,
+            "claim_amount": "55.6",
+            "car_make": "A",
+            "datetime": date(2021, 3, 15),
+        },
+    ]
+    exec_table_mock = mock.Mock(name="ExecTableMock", spec=["read_all"])
+    exec_table_mock.read_all.return_value = executor_output
+    tr = TableResultReader(table_columns)
+    result = list(tr.read_all_rows(exec_table_mock))
+
+    assert len(result) == len(expected)
+    for result_row, expected_row in zip(result, expected):
+        assert result_row == expected_row
+
+
+def test_insight_table_result_reader(table_columns):
+    executor_output = [
+        {"1": 125.5, "2": "55.6", "3": "A", "4": "2021-03-15"},
+    ]
+    query_columns = ["coverage_lifetime", "car_make", "datetime"]
+    expected = [
+        {"coverage_lifetime": 125.5, "car_make": "A", "datetime": date(2021, 3, 15)},
+    ]
+    exec_table_mock = mock.Mock(name="ExecTableMock", spec=["read_all"])
+    exec_table_mock.read_all.return_value = executor_output
+    tr = InsightTableResultReader(table_columns, query_columns)
+    result = list(tr.read_all_rows(exec_table_mock))
+
+    assert len(result) == len(expected)
+    for result_row, expected_row in zip(result, expected):
+        assert result_row == expected_row
