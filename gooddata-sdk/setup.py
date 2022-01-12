1c5ad15a0df13b6eeccdccfdaf0967f846665c31
Pavel Cerny <pavel.cerny@gooddata.com>
2022-01-12 21:35:10 +0100

    NAS-2888 add license to rest of projects

--- gooddata-sdk/setup.py
+++ gooddata-sdk/setup.py
@@ -1,5 +1,10 @@
 # (C) 2021 GoodData Corporation
+from pathlib import Path
+
 from setuptools import find_packages, setup
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text(encoding="utf-8")
 
 REQUIRES = [
     "gooddata-afm-client~=0.6.0",
@@ -13,12 +18,21 @@ REQUIRES = [
 setup(
     name="gooddata-sdk",
     description="GoodData.CN Python SDK",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     version="0.6.0",
     author="GoodData",
+    author_email="support@gooddata.com",
     license="MIT",
+    license_file="LICENSE.txt",
+    license_files=("LICENSE.txt",),
     install_requires=REQUIRES,
     packages=find_packages(exclude=["tests"]),
     python_requires=">=3.7.0",
+    project_urls={
+        "Documentation": "https://gooddata-sdk.readthedocs.io",
+        "Source": "https://github.com/gooddata/gooddata-python-sdk",
+    },
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Environment :: Console",
