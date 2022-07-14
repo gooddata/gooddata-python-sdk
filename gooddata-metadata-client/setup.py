002773b93b8a087cd84296e1ee3a75f91750e8a3
hkad98 <kadlino98@seznam.cz>
2022-07-14 14:38:32 +0200

    Bump to 1.0.0

--- gooddata-metadata-client/setup.py
+++ gooddata-metadata-client/setup.py
@@ -16,7 +16,7 @@ this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text(encoding="utf-8")
 
 NAME = "gooddata-metadata-client"
-VERSION = "0.8.0"
+VERSION = "1.0.0"
 # To install the library, run the following
 #
 # python setup.py install
