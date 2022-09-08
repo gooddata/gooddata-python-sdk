1cff88fc1a30c0242133f3b076efacae1750ab2c
hkad98 <kadlino98@seznam.cz>
2022-09-08 13:44:09 +0200

    Bump to 1.1.0

--- gooddata-scan-client/setup.py
+++ gooddata-scan-client/setup.py
@@ -16,7 +16,7 @@ this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text(encoding="utf-8")
 
 NAME = "gooddata-scan-client"
-VERSION = "1.0.0"
+VERSION = "1.1.0"
 # To install the library, run the following
 #
 # python setup.py install
