727a4f34b7607f17ca5a698b4201d889a4e176d1
Lubo Slivka <lubomir.slivka@gooddata.com>
2024-09-23 16:59:43 +0200

    chore: ditch custom flexfun result impl
    - instead use factory
    
    JIRA: CQ-670

--- gooddata-flexfun/gooddata_flexfun/flexfun/flex_fun_task.py
+++ gooddata-flexfun/gooddata_flexfun/flexfun/flex_fun_task.py
@@ -1,40 +1,12 @@
 #  (C) 2024 GoodData Corporation
-from collections.abc import Iterable
 from typing import Optional, Union
 
-import pyarrow
 import structlog
-from gooddata_flight_server import ArrowData, FlightDataTaskResult, Task, TaskError, TaskResult
+from gooddata_flight_server import FlightDataTaskResult, Task, TaskError, TaskResult
 
 from gooddata_flexfun.flexfun.flex_fun import FlexFun
 
 _LOGGER = structlog.get_logger("gooddata_flexfun.task")
-
-
-class _FlexFunResult(FlightDataTaskResult):
-    __slots__ = ("_result",)
-
-    def __init__(self, result: ArrowData) -> None:
-        # if function returns result as a reader, then naturally it can only be
-        # consumed once.
-        #
-        # on the other hand, if the result is table, it can be read as long as
-        # the result is present in the system (influenced by result TTL setting)
-        super().__init__(single_use_data=isinstance(result, pyarrow.RecordBatchReader))
-
-        self._result = result
-
-    def get_schema(self) -> pyarrow.Schema:
-        return self._result.schema
-
-    def _get_data(self) -> Union[Iterable[ArrowData], ArrowData]:
-        return self._result
-
-    def _close(self) -> None:
-        if isinstance(self._result, pyarrow.RecordBatchReader):
-            self._result.close()
-
-        self._result = None
 
 
 class FlexFunTask(Task):
@@ -73,7 +45,7 @@ class FlexFunTask(Task):
             headers=self._headers,
         )
 
-        return _FlexFunResult(result)
+        return FlightDataTaskResult.for_data(result)
 
     def on_task_cancel(self) -> None:
         _LOGGER.info("flexfun_task_cancel", fun=self._fun.Name, task_id=self._task_id)
