870a5d41be0e1318882faf5972e6a331c6f6264a
tubt <tu.bui@gooddata.com>
2024-12-06 21:27:18 +0700

    feat: POC automation test
    JIRA: QA-23477
    risk: nonprod

---
+++ gooddata-sdk/integration_tests/env.py
@@ -0,0 +1,9 @@
+# (C) 2024 GoodData Corporation
+# env.py
+import os
+
+# Define environment variables
+HOST = os.getenv("GOODDATA_HOST", "xxx")
+TOKEN = os.getenv("GOODDATA_TOKEN", "xxx")
+DATASOURCE_ID = os.getenv("DATASOURCE_ID", "xxx")
+WORKSPACE_ID = "xxx"
