43774304bb36a44a734cc3f7d85fd6f63cc1f1e8
Jan Kadlec <kadlino98@seznam.cz>
2025-02-18 20:06:35 +0100

    docs: add docstrings DataSourceValidator
    JIRA: TRIVIAL
    risk: low

--- gooddata-sdk/gooddata_sdk/catalog/data_source/validation/data_source.py
+++ gooddata-sdk/gooddata_sdk/catalog/data_source/validation/data_source.py
@@ -10,6 +10,13 @@ class DataSourceValidator:
         self._ds_service = data_source_service
 
     def validate_ldm(self, model: CatalogDeclarativeModel) -> None:
+        """
+        Validates that referenced data sources in LDM actually exist.
+        If they do not exist, raises ValueError.
+
+        Returns:
+            None
+        """
         if model.ldm is not None:
             ldm_ds_in_use = set(
                 dataset.data_source_table_id.data_source_id
@@ -19,6 +26,12 @@ class DataSourceValidator:
             self.validate_data_source_ids(ldm_ds_in_use)
 
     def validate_data_source_ids(self, data_source_ids: set[str]) -> None:
+        """
+        Compares data source ids with existing data sources in the catalog.
+
+        Returns:
+            None
+        """
         full_ds_set = set(data_source.id for data_source in self._ds_service.list_data_sources())
         diff_ds = data_source_ids - full_ds_set
         if len(diff_ds) != 0:
