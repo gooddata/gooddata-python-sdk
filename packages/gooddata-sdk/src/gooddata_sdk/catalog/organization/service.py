92f05917e4fbbee4adc1572bdfd6df6bb2171ee7
Jan Kadlec <jan.kadlec@gooddata.com>
2025-11-03 11:26:44 +0100

    fix: remove `update_oidc_parameters`
    There was a breaking change on the backend that rendered `update_oidc_parameters` unusable. OIDC was moved from the organization to the IdentityProvider entity; therefore, this API stopped working. Use `update_identity_provider` instead.

--- packages/gooddata-sdk/src/gooddata_sdk/catalog/organization/service.py
+++ packages/gooddata-sdk/src/gooddata_sdk/catalog/organization/service.py
@@ -41,40 +41,6 @@ class CatalogOrganizationService(CatalogServiceBase):
 
     # Entities API
 
-    def update_oidc_parameters(
-        self,
-        oauth_issuer_location: Optional[str] = None,
-        oauth_client_id: Optional[str] = None,
-        oauth_client_secret: Optional[str] = None,
-    ) -> None:
-        """Updates OIDC parameters of organization.
-
-        Args:
-            oauth_issuer_location (Optional[str], optional):
-                Issuer location. Defaults to None.
-            oauth_client_id (Optional[str], optional):
-                Public client identifier. Defaults to None.
-            oauth_client_secret (Optional[str], optional):
-                Client secret. Defaults to None.
-
-        Returns:
-            None
-
-        Raises:
-            ValueError:
-                Parameters were not strictly all none or all string.
-        """
-        parameters = [oauth_issuer_location, oauth_client_id, oauth_client_secret]
-        if not all(p is not None for p in parameters) and any(p is not None for p in parameters):
-            raise ValueError("All parameters have to be set to None or all parameters has to be string.")
-        organization = self.get_organization()
-        organization.attributes.oauth_issuer_location = oauth_issuer_location
-        organization.attributes.oauth_client_id = oauth_client_id
-        organization_document = CatalogOrganizationDocument(data=organization)
-        self._entities_api.update_entity_organizations(
-            organization.id, organization_document.to_api(oauth_client_secret=oauth_client_secret)
-        )
-
     def update_name(self, name: str) -> None:
         """Updates the name of the organization.
 
